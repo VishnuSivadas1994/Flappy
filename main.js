2a336f9c38f5853f3e0b43895d5e62a11ba2c7bd
VishnuSivadasa1994 <vishnushivadas2007@gmail.com>
2021-03-04 17:43:25 -0500

    collision

--- main.js
+++ main.js
@@ -10,15 +10,30 @@ let frame = 0;
 let score = 0;
 let gameSpeed = 2;
 
+const gradient = ctx.createLinearGradient(0, 0, 0, 70);
+gradient.addColorStop('0.4', '#fff');
+gradient.addColorStop('0.5', '#000');
+gradient.addColorStop('0.55', '#4040ff');
+gradient.addColorStop('0.6', '#000');
+gradient.addColorStop('0.9', '#fff');
+
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //ctx.fillRect(10, canvas.height - 90, 50, 50);
     handleObstacles();
+    handleParticles();
 
     bird.update();
     bird.draw();
-    handleParticles();
+
+    ctx.fillStyle = gradient;
+    ctx.font = '90px Geogia';
+    ctx.strokeText(score, 450, 70);
+    ctx.fillText(score, 450, 70);
+
+    handleCollisions();
+    if (handleCollisions()) return;
     requestAnimationFrame(animate);
     angle += 0.12;
     hue++;
@@ -35,3 +50,22 @@ window.addEventListener('keyup', function (e) {
     console.log(e.code);
     if (e.code === 'Space') spacePressed = false;
 });
+
+
+const bang = new Image();
+bang.src = 'download.png';
+function handleCollisions() {
+    for (let i = 0; i < obstaclesArray.length; i++) {
+        if (bird.x < obstaclesArray[i].x + obstaclesArray[i].width &&
+            bird.x + bird.width > obstaclesArray[i].x &&
+            ((bird.y < 0 + obstaclesArray[i].top && bird.y + bird.height > 0) ||
+                (bird.y > canvas.height - obstaclesArray[i].bottom &&
+                    bird.y + bird.height < canvas.height))) {
+            ctx.drawImage(bang, bird.x, bird.y, 50, 50);
+            ctx.font = "25px Georgia";
+            ctx.fillStyle = 'black';
+            ctx.fillText('Game Over, your score is ' + score, 160, canvas.height / 2 - 10);
+            return true;
+        }
+    }
+}
